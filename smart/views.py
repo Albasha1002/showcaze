22ae1f303577735240eb786acd5b9009d3e0fbc2
basha <basha.kkl1999@gmail.com>
2021-06-08 12:38:27 +0530

    add requirements.txt

--- smart/views.py
+++ smart/views.py
@@ -1,7 +1,15 @@
+import requestsp
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def home(request):
     return render (request, 'base.html')
+
+
+def searchthink(request):
+    mysearch= request.POST.get('searchthink')
+    mysearch1={'searchthink':mysearch}
+    return render (request, 'my_web/new_search.html', mysearch1)
